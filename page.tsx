4dbbe150f1d12e513d0f5a5e0a58a13e7bfe1cff
recyclingmonkey <info@recyclingmonkey.co.uk>
2025-04-27 00:02:11 +0100

    Add files via upload

---
+++ page.tsx
@@ -0,0 +1,10 @@
+export default function ScrapMyCar() {
+  return (
+    <div className="p-8">
+      <h1 className="text-3xl font-bold">Scrap My Car</h1>
+      <p className="mt-4">
+        Get an instant offer for your scrap car anywhere in the UK!
+      </p>
+    </div>
+  );
+}
